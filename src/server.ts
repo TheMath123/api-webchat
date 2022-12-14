a5cb512bf9cdaa5c937f13a4af595fc33f4d39f1
Matheus P Agostinho <contact@matheuspa.me>
2022-12-14 20:45:41 -0300

    refac: remove death code

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ const corsOptions: CorsOptions = {
   origin: process.env.URL_APP || '*',
   allowedHeaders: '*',
   methods: '*', // some legacy browsers (IE11, various SmartTVs) choke on 204
-  credentials: true,
+  // credentials: true,
 };
 
 const httpServer = createServer(app);
@@ -28,7 +28,6 @@ const io = new Server(httpServer, {
   path: '/chat-connection/',
   cors: corsOptions,
   connectTimeout: 300,
-  transports: ['websocket'],
 });
 
 io.on('connection', socket => {
